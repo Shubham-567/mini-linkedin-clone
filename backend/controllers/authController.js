cd69eeb45172ec7110d4b39e35680f7b48c9f968
Shubham <shubhampatil1356@gmail.com>
2025-08-02 22:53:32 +0530

    Backend: Add login route with password verification and JWT

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -44,3 +44,38 @@ export const registerUser = async (req, res) => {
     res.status(500).json({ message: "Server error during user registration" });
   }
 };
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Checking if user exits
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    // Comparing passwords
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    // Generating token
+    const token = generateToken(user._id);
+
+    // Return user data + token
+    res.status(200).json({
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        bio: user.bio,
+      },
+      token,
+    });
+  } catch (error) {
+    console.error("Login error: ", error);
+    res.status(500).json({ message: "Server error during login" });
+  }
+};
