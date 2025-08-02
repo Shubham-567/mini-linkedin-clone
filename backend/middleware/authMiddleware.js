e54cf76b5d1f5139c51d2bcbe989295eb2ac73b5
Shubham <shubhampatil1356@gmail.com>
2025-08-02 23:20:23 +0530

    Backend: Add JWT auth middleware to protect private routes

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,34 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User";
+
+const protect = async (req, res, next) => {
+  let token;
+
+  // Checking for token in authorization header: Bearer <token>
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      // Extracting token
+      token = req.headers.authorization.split(" ")[1];
+
+      // verifying token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      // Attach user to request
+      req.user = await User.findById(decoded.id).select("-password"); // password is excluded
+
+      next();
+    } catch (error) {
+      console.error("JWT auth failed: ", error);
+      return res.status(401).json({ message: "Not authorized, token failed" });
+    }
+  } else {
+    return res
+      .status(401)
+      .json({ message: "Not authorized, no token provided" });
+  }
+};
+
+export default protect;
