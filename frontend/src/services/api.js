4be2aacadcd6934ac140033162eef8598ab76fbd
Shubham <shubhampatil1356@gmail.com>
2025-08-03 12:52:12 +0530

    Frontend: Add axios setup and api services files for auth, posts, users

---
+++ frontend/src/services/api.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import useAuthStore from "../store/authStore";
+
+const BASE_URL = import.meta.env.VITE_API_URL;
+
+// creating axios instance
+const api = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Attach token automatically before each request
+api.interceptors.request.use(
+  (config) => {
+    const token = useAuthStore.getState().token;
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+
+  (error) => Promise.reject(error)
+);
+
+export default api;
---
+++ frontend/src/services/postService.js
@@ -0,0 +1,11 @@
+import api from "./api";
+
+export const createPost = async (text) => {
+  const res = await api.post("/posts", { text });
+  return res.data;
+};
+
+export const getAllPost = async () => {
+  const res = await api.get("/posts");
+  return res.data;
+};
